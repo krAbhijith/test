a0574394b83cc702bf9565284820a7c13da90e3d
Abhijith K R <abhijithkr.8005@gmail.com>
2025-03-05 23:00:24 +0530

    changed

--- sanity.cli.js
+++ sanity.cli.js
@@ -12,3 +12,5 @@ export default defineCliConfig({
   autoUpdates: true,
   studioHost: 'test1289'
 })
+
+// skbyN45QaZvFijLzIQ5bYZQHssrrrsTJd0U3pMYJs8Eu11lFm5b16BQj1ldmVicRbJzNrKK0w9CzshWlXRDZSpE4rwVbbVdvKxw1hx8AWdaSgYbKBnR4zBEkzrfA4j147x97pOUxhG9VbWa7Hlfvi4B44Gd52DlQ2PZD7iss6DB2Ux9CepfB
